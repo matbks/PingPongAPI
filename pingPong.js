378adb133b4aba4f0ff62cf9fddc3f62f82db47f
Backhaus <44726485+matbks@users.noreply.github.com>
2023-10-02 16:44:22 -0300

    sssss

--- pingPong.js
+++ pingPong.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = 3002; 
+const port = 3003; 
 
 // Endpoint que responde com "pong" quando recebe um "ping"
 app.post('/ping', (req, res) => {
