b9011a9dc772e2de7d0e85c72e8a5e2a96d7d730
Backhaus <44726485+matbks@users.noreply.github.com>
2023-10-04 16:16:53 -0300

    ss

--- pingPong.js
+++ pingPong.js
@@ -3,10 +3,13 @@ const axios = require('axios');
 const app = express();
 const port = 3002; 
 
+// Middleware para tratar JSON
+app.use(express.json());
+
 // Endpoint que responde com "pong" quando recebe um "ping"
 app.post('/ping', (req, res) => {
-    response = '{ "data" : "pong" }'
-    res.send(response);
+    const response = { data: "pong" }; // Objeto JavaScript, não uma string
+    res.json(response); // Usando o método .json() para enviar como resposta
 });
 
 app.listen(port, () => {
